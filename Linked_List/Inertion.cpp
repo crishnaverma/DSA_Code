341950c5af1dc5a4e0630f6618dc76561f931ab9
Krishna Verma <crishnaverma@gmail.com>
2026-09-16 21:13:16 +0530

    insertion

---
+++ Linked_List/Inertion.cpp
@@ -0,0 +1,46 @@
+/*
+Question:-
+Insert a node at the beginning of a singly linked list.
+Hint: Think about which pointer needs to change first so you don't lose the rest of the list.
+Input List: 2 -> 3 -> 4, Value to insert: 1
+Output 1 -> 2 -> 3 -> 4
+*/
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data,Node*ptr)
+    {
+        this->data = data;
+        this->next = ptr;
+    }
+};
+
+Node* Linkedlist(vector<int>& arr)
+{
+    Node* head = new Node(arr[0],nullptr);
+    Node* temp = head;
+    for(int i =1;i<arr.size();i++)
+    {
+        Node* newNode = new Node(arr[i], nullptr);
+        temp->next = newNode;
+        temp = newNode;
+    }
+    return head;
+}
+
+Node*  Insertion_at_first(Node* head,int value)
+{
+    Node* node1 = new Node(value,head);
+    head = node1;
+    return head;
+    
+}
