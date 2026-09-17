e745b2124e792e7d1aceae1139bc6961ded8d99c
Krishna Verma <crishnaverma@gmail.com>
2026-09-18 00:08:46 +0530

    searching

---
+++ Linked_List/Searching.cpp
@@ -0,0 +1,7 @@
+/*
+ Search for a given value in a linked list and return its position.
+Hint: Return -1 (or similar) if not found.
+Input List: 4 -> 9 -> 2 -> 6, Target: 2
+Output 2 (0-indexed position)
+*/
+
